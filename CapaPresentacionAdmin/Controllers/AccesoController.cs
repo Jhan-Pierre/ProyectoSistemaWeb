03da222b1015e3b5aa257c6df52ca59c2cc885ea
JHANPIERRE <jmunozmanayay@gmail.com>
2025-03-01 09:35:16 -0500

    Se añadio la funcionalidad para Cerrar Sesion y Proteger las rutas.

--- CapaPresentacionAdmin/Controllers/AccesoController.cs
+++ CapaPresentacionAdmin/Controllers/AccesoController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Security;
 using CapaEntidad;
 using CapaNegocio;
 
@@ -46,6 +46,8 @@ namespace CapaPresentacionAdmin.Controllers
                     TempData["IdUsuario"] = oUsuario.IdUsuario;
                     return RedirectToAction("CambiarClave");
                 }
+
+                FormsAuthentication.SetAuthCookie(oUsuario.Correo, false);
 
                 ViewBag.Error = null;
                 return RedirectToAction("Index","Home");
@@ -122,6 +124,13 @@ namespace CapaPresentacionAdmin.Controllers
                 ViewBag.Error = mensaje;
                 return View();
             }
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Acceso");
+
         }
     }
 }
