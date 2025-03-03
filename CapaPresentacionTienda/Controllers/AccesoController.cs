bdae051a234469c3eacf237584a47f51db52dd76
Fabian Ambrosio <ambrosiofabian10@gmail.com>
2025-03-03 03:12:41 -0500

    Se hizo la interfaz de la tienda, se añadio el carrito

--- CapaPresentacionTienda/Controllers/AccesoController.cs
+++ CapaPresentacionTienda/Controllers/AccesoController.cs
@@ -166,6 +166,7 @@ namespace CapaPresentacionTienda.Controllers
 
         public ActionResult CerrarSesion()
         {
+            Session["Cliente"] = null;
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Acceso");
 
