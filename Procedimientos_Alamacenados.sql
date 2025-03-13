1a31176762c7466dbea75a36bab02a5779d1036b
Fabian Ambrosio <ambrosiofabian10@gmail.com>
2025-03-12 20:50:51 -0500

    Se hizo funcional el boton de pago

--- Procedimientos_Alamacenados.sql
+++ Procedimientos_Alamacenados.sql
@@ -471,3 +471,60 @@ begin
 	END CATCH
 
 end
+
+
+
+select * from cliente
+
+
+CREATE TYPE [dbo].[Edetalle_Venta] AS TABLE(
+	[IdProducto] int null,
+	[Cantidad] int null,
+	[Total] decimal (18,2) null
+)
+
+GO
+
+create procedure usp_ResgistrarVenta(
+@IdCliente int,
+@TotalProducto int,
+@MontoTotal decimal (18,2),
+@Contacto varchar(100),
+@IdDistrito varchar (6),
+@Telefono varchar (10),
+@Direccion varchar (100),
+@IdTransaccion varchar (50),
+@DetalleVenta [EDetalle_Venta] READONLY,
+@Resultado bit output,
+@Mensaje varchar(500) output
+)
+as
+begin
+
+	begin try
+
+		declare @idventa int = 0
+		set @Resultado = 1
+		set @Mensaje = ''
+
+		begin transaction registro
+
+		insert into VENTA(IdCliente,TotalProducto,MontoTotal,Contacto,IdDistrito,Telefono,Direccion,IdTransaccion)
+		values(@IdCliente,@TotalProducto,@MontoTotal,@Contacto,@IdDistrito,@Telefono,@Direccion,@IdTransaccion)
+
+		set @idventa = SCOPE_IDENTITY()
+
+		insert into DETALLE_VENTA(IdVenta,IdProducto,Cantidad,Total)
+		select @idventa,IdProducto,Cantidad,Total from @DetalleVenta
+
+		DELETE FROM CARRITO WHERE IdCliente = @IdCliente
+
+		commit transaction registro
+
+	end try
+	begin catch
+		set @Resultado = 0
+		set @Mensaje = ERROR_MESSAGE()
+		rollback transaction registro
+	end catch
+end
